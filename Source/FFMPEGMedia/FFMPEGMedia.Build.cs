e57ff093a8f4f37b61170b0f3790686d8c9d81c6
Matrix <35943087+zwq33355555@users.noreply.github.com>
2020-10-15 16:14:43 +0800

    Revert "Upgrade FFMPEG to version 4.3.1"
    This reverts commit c5c42c89ba611b51caf27f74d6c8f6f4374e551f.

--- Source/FFMPEGMedia/FFMPEGMedia.Build.cs
+++ Source/FFMPEGMedia/FFMPEGMedia.Build.cs
@@ -21,21 +21,23 @@ public class FFMPEGMedia : ModuleRules
 		get { return Directory.GetParent(ModulePath).Parent.FullName; }
 	}
 
-        string filename = Path.GetFileName(Filepath);
 
 	private void CopyToBinaries(string Filepath, ReadOnlyTargetRules Target)
 	{
-        System.Console.WriteLine("Writing file " + Filepath + " to " + binariesDir);
+		string binariesDir = Path.Combine(UProjectPath, "Binaries", Target.Platform.ToString());
+		string filename = Path.GetFileName(Filepath);
 
-        if (!Directory.Exists(binariesDir))
-            Directory.CreateDirectory(binariesDir);
+		System.Console.WriteLine("Writing file " + Filepath + " to " + binariesDir);
 
-        if (!File.Exists(Path.Combine(binariesDir, filename)))
-            File.Copy(Filepath, Path.Combine(binariesDir, filename), true);
-    }
-	*/
+		if (!Directory.Exists(binariesDir))
+			Directory.CreateDirectory(binariesDir);
 
-    public bool LoadFFmpeg(ReadOnlyTargetRules Target)
+		if (!File.Exists(Path.Combine(binariesDir, filename)))
+			File.Copy(Filepath, Path.Combine(binariesDir, filename), true);
+	}
+
+
+	public bool LoadFFmpeg(ReadOnlyTargetRules Target)
 	{
 		bool isLibrarySupported = false;
 
@@ -61,14 +63,11 @@ public class FFMPEGMedia : ModuleRules
 			string[] dlls = {"avcodec-58.dll","avdevice-58.dll", "avfilter-7.dll", "avformat-58.dll", "avutil-56.dll", "swresample-3.dll", "swscale-5.dll", "postproc-55.dll"};
 
 			string BinariesPath = Path.Combine(Path.Combine(Path.Combine(ThirdPartyPath, "ffmpeg", "bin"), "vs"), PlatformString);
-			System.Console.WriteLine("... BinariesPath -> " + BinariesPath);
 			foreach (string dll in dlls)
 			{
-				/*  //New Version FFMPEG DLLs not support Delay-Load
 				PublicDelayLoadDLLs.Add(dll);
-				RuntimeDependencies.Add(Path.Combine(BinariesPath, dll), StagedFileType.NonUFS);	
-				*/
-				RuntimeDependencies.Add("$(BinaryOutputDir)/" + dll, Path.Combine(BinariesPath, dll));
+				//CopyToBinaries(Path.Combine(BinariesPath, dll), Target);
+				RuntimeDependencies.Add(Path.Combine(BinariesPath, dll), StagedFileType.NonUFS);
 			}
 
 		}
